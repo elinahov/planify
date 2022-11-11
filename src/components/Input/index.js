a4501bcf6a600b37f17aa8bf6965c7ed9e4e1584
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-11-11 20:33:55 +0400

    Adding tasks to firestore

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,11 +3,11 @@ import {TextInput} from 'react-native';
 import styles from './styles';
 import colors from '../../constants/colors';
 
-const Input = ({...props}) => {
+const Input = ({outlined, ...props}) => {
   return (
     <TextInput
       placeholderTextColor={colors.midGrey}
-      style={styles.input}
+      style={[styles.input, outlined ? styles.outlined : {}]}
       {...props}
     />
   );
