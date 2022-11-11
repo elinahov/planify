1fba61bee687e7338e471e8140f74bd737325d01
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2022-11-11 15:10:26 +0400

    Setup firebase, auth with firebase

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Onboarding from './src/screens/auth/Onboarding';
-import Signin from './src/screens/auth/Signin';
-import Signup from './src/screens/auth/Signup';
-
-const Stack = createStackNavigator();
+import Routes from './src/Routes';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Signin" component={Signin} />
-        <Stack.Screen name="Signup" component={Signup} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 };
